4f79e7fc80e4fb7811dd600d56c622e0231bb624
aphy18 <79496374+aphy18@users.noreply.github.com>
2021-06-22 00:51:28 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,16 +1,3 @@
-function assertEqual(actual,expected){
-    const pass = String.fromCodePoint(0x274C);
-    const fail = String.fromCodePoint(0x274E)
-    if(actual === expected){
-        console.log(`${pass} Assertion Passed:[actual] === [expected]`)
-    }
-
-    else{
-        console.log(`${fail} Assertion Failed: [actual] !== [expected]`)
-    }
-}
-
-
 function head(array){
     if(array){
         console.log(array[0]);
