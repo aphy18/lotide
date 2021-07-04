4f9fe726002384b3f03bbbe93f3a999f54490b5e
aphy18 <aphason1@.com>
2021-07-04 16:58:49 +0000

    accessing value from object

---
+++ findKey.js
@@ -0,0 +1,27 @@
+
+
+function findKey(object, callback) {
+    for (let key of Object.keys(object)) {
+        let value = object[key];
+        if (callback(value)) {
+            console.log(key)
+            return key;
+
+        };
+    };
+    return undefined;
+};
+
+
+findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+}, x => x.stars === 3) // => "noma"
+
+
+
+
