c374e2ad5a5b79ad484c0af56c5d7d93d1519711
aphy18 <aphason1@.com>
2021-07-21 03:12:04 +0000

    made format changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,23 @@
-function eqlArray(arr1, arr2) {
-    let value = true
-    if(arr1.length !== arr2.length){
-        value = false;
+function assertArraysEqual(arr1, arr2) {
+  let value = true;
+  if (arr1.length !== arr2.length) {
+    value = false;
+  }
+  arr1.forEach((element,index) => {
+    if (element !== arr2[index]) {
+      value = false;
     }
-    arr1.forEach((element,index) => {
-        if(element !== arr2[index]){
-            value = false;
-        }
-    });
+  });
 
-    if(value === true){
-        console.log("The arrays are equal!");
-    }
-
-    else{
-        console.log("The arrays are not equal!");
-    }
+  if (value === true) {
+    console.log("These arrays are equal.");
+  } else {
+    console.log("These arrays are not equal.");
+  }
 }
 
+assertArraysEqual([1,2,4],[1,2,4]);
+
+module.exports = {
+  assertArraysEqual
+};
