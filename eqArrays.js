d9a81d8f1e404c358183d91a82a012b242acfdea
aphy18 <aphason1@.com>
2021-07-26 17:34:53 +0000

    refactored and changed the assertEqual variable

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./test/assertEqualTest.test.js');
+
 function eqArrays(arr1, arr2) {
   let value = true;
   if (arr1.length !== arr2.length) {
@@ -8,12 +10,11 @@ function eqArrays(arr1, arr2) {
       value = false;
     }
   });
-
+  console.log(value)
   return value;
 }
 
-// eqArrays([1,2,3],[1,2,3])
+assertEqual(eqArrays([1,2,3],[1,2,3,4]), false);
+assertEqual(eqArrays([2,4,6],[2,4,6]), true);
 
-module.exports = {
-  eqArrays
-};
+module.exports = eqArrays;
