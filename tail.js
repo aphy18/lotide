321090cc65d93ce57ce8c5c42c52154b0d57b247
aphy18 <aphason1@.com>
2021-07-26 17:21:11 +0000

    made fn an arrow fn

--- tail.js
+++ tail.js
@@ -1,9 +1,21 @@
-function tail(array) {
-    if (array) {
-        console.log(array.slice(1))
-    }
+const assertEqual = require('./test/assertEqualTest.test.js');
 
-    else {
-        console.log(undefined)
-    }
-}
+const tail = arr => {
+  if (!arr) {
+    return undefined;
+  }
+  return arr.slice(1);
+};
+
+//in JS two different arrays will never be equal to each other due to them having different "addresses"
+
+
+const words = ["Lighthouse", "Labs"];
+assertEqual(words.length, 2);
+
+const result = tail([1,2,3,4,5]); // saving the function inside a variable
+assertEqual(result[0],2);
+
+
+  
+module.exports = tail;
