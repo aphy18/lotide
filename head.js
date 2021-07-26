1d62ce818b454be96399529dd82ebfa3338af26c
aphy18 <aphason1@.com>
2021-07-26 17:20:44 +0000

    made fn an arrow fn

--- head.js
+++ head.js
@@ -1,12 +1,12 @@
 const assertEqual = require('./test/assertEqualTest.test.js');
 
-function head(arr) {
+const head = arr => {
   if (!arr) {
     return undefined;
   }
   return arr[0];
-}
+};
 
-assertEqual(head([1,2,3]),1)
+assertEqual(head([1,2,3]),1);
 
 module.exports = head;
