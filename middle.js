1e17a07693f801b326557f12b500e01d0e14b73e
aphy18 <aphason1@.com>
2021-07-26 21:35:35 +0000

    refactored

--- middle.js
+++ middle.js
@@ -1,32 +1,21 @@
-// create a function that returns the middle element of an array
+const middle = arr => {
+  const newArr = [];
+  const middleIndex = Math.floor((arr.length / 2));
+  if (arr.length % 2 === 0) {
+    newArr.push(arr[middleIndex - 1], arr[middleIndex]);
+  } else {
+    newArr.push(arr[middleIndex]);
+  }
+  return newArr;
+};
 
-// how can we access elements: forEach, forOf, for loop
 
-// how do we access the middle element?
+module.exports = middle;
+     
 
-function middle(arr) {
-    // going to push 'arr' elements in newArr
-    let newArr = [];
-    // how to determine middle index
-    let middleIndex = Math.floor((arr.length / 2))
-    // if the length of the array is divisble by 2
-    if(arr.length % 2 === 0) {
-        newArr.push(arr[middleIndex-1], arr[middleIndex]);
-        
-    }
-
-    else {
-        newArr.push(arr[middleIndex]);
-    }
+   
 
   
-
-    return newArr;
-}
-
-    console.log(middle([34,3,56,51,12,65]))
-
-
 
 
 
